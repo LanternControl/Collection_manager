831e65076fa1600b20e8ad8bbb6ee2117296eb55
LanternControl <brett4president@hotmail.com>
2017-09-04 23:00:12 -0400

    Think I've got all my routes, now to work on my mustache files

--- app.js
+++ app.js
@@ -17,3 +17,59 @@ app.use(bodyParser.urlencoded({ extended:true }));
 app.engine('mustache', mustache());
 app.set('views', './views');
 app.set('view engine', 'mustache');
+
+app.get('/add', function(req, res){
+  res.render('add');
+});
+
+app.post('/add', function(req, res){
+  Magic_cards.create.(req.body).then(function(magic_cards){
+    res.redirect('/');
+  });
+});
+
+app.get('/:id', function(req, res) {
+ Magic_cards.find({
+  _id: req.params.id
+}).then(function(magic_cards) {
+  res.render('id', {magic_cards: magic_cards});
+ });
+});
+
+app.post('/:id', function(req, res) {
+ Magic_cards.findByIdAndRemove({
+  _id: req.params.id
+}).then(function(magic_cards) {
+  res.redirect('/');
+});
+});
+
+
+app.get('/:id/edit', function(req, res) {
+ Books.find({
+  _id: req.params.id
+}).then(function(magic_cards) {
+  res.render('edit', {magic_cards: magic_cards});
+ });
+});
+
+app.post('/:id/edit', function(req, res) {
+ Magic_cards.findOneAndUpdate({
+  _id: req.params.id
+}, req.body).then(function(magic_cards) {
+  res.redirect('/')
+ });
+});
+
+
+app.get('/', function(req, res){
+  Magic_cards.find().then function(magic_cards) {
+    res.render('index', {
+      magic_cards: magic_cards
+    });
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Successfully started express application!');
+});
